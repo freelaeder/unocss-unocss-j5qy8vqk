fac9dfc4f8081b82c18646126c0ab52b7294ed80
freelaeder <yanglo67@qq.com>
2024-12-30 16:29:14 +0800

    移除 EffectSwiper 组件的 CSS 文件，优化图片加载逻辑并更新图片参数颜色

--- src/views/components/EffectSwiper.tsx
+++ src/views/components/EffectSwiper.tsx
@@ -4,7 +4,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-cards';
-import './EffectSwiper.css';
 // import required modules
 import { EffectCards } from 'swiper/modules';
 
@@ -12,33 +11,52 @@ export default function App() {
 
   // 图片路径集合
   const [images, setImages] = useState([]);
-  // 图片名称及颜色
+  // 图片名称及颜色参数
   const imageParams = [
-    { name: '戴帽子像素人', color: 'red' },
-    { name: '方块人', color: 'green' },
-    { name: '方块人杂技', color: 'blue' },
-    { name: '可爱像素人', color: 'yellow' },
-    { name: '像素巫师', color: 'purple' },
-    { name: '像素宝箱', color: '' },
-    { name: '方块魔法师', color: '' },
-    { name: '像素人', color: '' },
-    { name: '魔法师像素人', color: '' },
-    { name: '方块人那套索', color: '' },
-    { name: '像素魔法巫师', color: '' },
-    { name: '像素人拿绳子', color: '' },
-    { name: '像素人拿宝箱', color: '' },
-    { name: '像素人拿剑', color: '' },
+    { name: '戴帽子像素人', color: 'rgb(206, 17, 17)' },
+    { name: '方块人', color: 'rgb(0, 140, 255)' },
+    { name: '方块人杂技', color: 'rgb(10, 184, 111)' },
+    { name: '可爱像素人', color: 'rgb(211, 122, 7)' },
+    { name: '像素巫师', color: 'rgb(118, 163, 12)' },
+    { name: '像素宝箱', color: 'rgb(180, 10, 47)' },
+    { name: '方块魔法师', color: 'rgb(35, 99, 19)' },
+    { name: '像素人', color: 'rgb(0, 68, 255)' },
+    { name: '魔法师像素人', color: 'rgb(218, 12, 218)' },
+    { name: '大力士小恶魔', color: 'rgb(255, 0, 0)' },
+    { name: '大笑小恶魔', color: 'rgb(255, 165, 0)' },
+    { name: '呆萌小恶魔', color: 'rgb(255, 255, 0)' },
+    { name: '可爱小恶麾', color: 'rgb(0, 128, 0)' },
+    { name: '恐怖小恶魔', color: 'rgb(0, 0, 255)' },
+    { name: '吸血小恶麾', color: 'rgb(75, 0, 130)' },
+    { name: '小恶魔艾玛', color: 'rgb(238, 130, 238)' },
+    { name: '小恶磨蝙蝠', color: 'rgb(128, 0, 0)' },
+    { name: '小恶魔查理', color: 'rgb(255, 192, 203)' },
+    { name: '小恶麾哈利', color: 'rgb(255, 69, 0)' },
+    { name: '小恶魔坏笑', color: 'rgb(34, 139, 34)' },
+    { name: '小恶魔杰克', color: 'rgb(0, 191, 255)' },
+    { name: '小恶魔卡尔', color: 'rgb(123, 104, 238)' },
+    { name: '小恶魔莱恩', color: 'rgb(255, 20, 147)' },
+    { name: '小恶麾丽莎', color: 'rgb(255, 105, 180)' },
+    { name: '小恶麾吓人', color: 'rgb(139, 0, 0)' },
+    { name: '小恶魔象鼻', color: 'rgb(0, 100, 0)' },
+    { name: '小恶魔小牛', color: 'rgb(54, 94, 77)' },
   ];
 
-  // Load images and JSON data
+  // 按顺序加载图片和 JSON 数据
   const loadImages = async () => {
     const imageModules = import.meta.glob('@/assets/images/*.svg');
-    const loadedImages = await Promise.all(
-      Object.values(imageModules).map(async (module) => {
-        const moduleResult = await module();
-        return moduleResult.default;
-      })
-    );
+    const imagePaths = Object.keys(imageModules).sort((a, b) => {
+      const numA = parseInt(a.match(/(\d+)\.svg$/)[1]);
+      const numB = parseInt(b.match(/(\d+)\.svg$/)[1]);
+      return numA - numB;
+    });
+
+    const loadedImages = [];
+    for (const path of imagePaths) {
+      const module = await imageModules[path]();
+      loadedImages.push(module.default);
+    }
+
     setImages(loadedImages);
   };
 
@@ -56,8 +74,8 @@ export default function App() {
       >
         {
           images.map((image, index) => (
-            <SwiperSlide key={index} className='flex flex-col'>
-              <img className='w-1/2 aspect-square' src={image} alt={`Image ${index}`} />
+            <SwiperSlide key={index} className='flex flex-col  items-center justify-center rounded-lg font-bold text-white' style={{ backgroundColor: imageParams[index].color }}>
+              <img className='w-1/2 aspect-square rounded' src={image} alt={`Image ${index}`} />
               <div>
                 <h3>
                   {
